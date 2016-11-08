b1a9387a0f4f2d7b7c7a3c6a3afcdc85bc3b451f
jbshuler <jbshuler@gmail.com>
2016-11-08 11:49:05 -0700

    HW8

--- Homework/08/GEOS597_HW8_DudunakeShuler.m
+++ Homework/08/GEOS597_HW8_DudunakeShuler.m
@@ -1,5 +1,11 @@
-%% Homework 7    
+%% Homework 8    
 % _*Taylor Dudunake & John Shuler*_
 %
+% GEOS597 Homework #8: GPS, streamflow data and statistics
+%
 % Due: 11/14/2016
-%%
+
+clear all;
+close all;
+clc;
+%% *Part 1: Automated GPS Data Processing*
