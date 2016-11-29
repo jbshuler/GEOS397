6d69f9a1be934b1e5568076fa81a1553a6adf574
jbshuler <JohnShuler@boisestate.edu>
2016-11-29 11:53:20 -0700

    Final ctd.

--- Homework/Final Project/GEOS597_FinalProject_Shuler.m
+++ Homework/Final Project/GEOS597_FinalProject_Shuler.m
@@ -7,3 +7,14 @@ clear all;
 clc;
 %% Import raw .dat files
 importDatafiles
+
+%% 
+
+fileID = fopen('./CR6Series_SoilPit1_2016_07_09.dat','r');
+C = textscan(fileID, '%s%s%d%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f', 'HeaderLines', 4,'TreatAsEmpty','NaN');
+fclose(fileID);
+
+TIMESTAMP = C{1};
+% For temp. influence on signal- Fitting Toolbox, 'nonlinfit'
+% Maybe low pass filter
+plot(C{4})
--- Homework/Final Project/importPitData.m
+++ Homework/Final Project/importPitData.m
@@ -1,10 +1,8 @@
 %% Import a single data file
 function  [] = importPitData(filename)
-    %fid = fopen( fullfile('H:\Classes_Teaching_Fall 2016\Computation\GEOS397\Homework\Final Project', filename), 'r' );
-    %fclose(fid);
-
-fileID=fopen(filename);
-C = textscan(fileID, '%f%f%f%f%f%f%f%f%f');
-fclose(fileID);
+  
+    fileID=fopen(filename);
+    C = textscan(fileID, '%f%f%f%f%f%f%f%f%f');
+    fclose(fileID);
 
 end
